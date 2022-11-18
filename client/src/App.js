ac7d3bf225d26e9b57f7d86f41f6bdb9746f54de
AmranElmasri <amran.man99@gmail.com>
2022-11-18 23:14:03 +0200

    handle manager config

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { ConfigureCache, GetForm, InsertForm, ListKeys, Statistics } from "./pages";
+import { ConfigureCache, GetForm, InsertForm, ListKeys, ManagerApp, Statistics } from "./pages";
 import axios from "./apis/index";
  
 
 function App() {
 
 
-  setInterval(async() => {
-    await axios.get('/update-statistics');
-  }, 5000);
+  // setInterval(async() => {
+  //   await axios.get('/update-statistics');
+  // }, 5000);
   
   return (
     <Sidebar>
@@ -20,6 +20,7 @@ function App() {
         <Route path="/listKeys" element={<ListKeys />} />
         <Route path="/mem-cache" element={<ConfigureCache/>} />
         <Route path="/statistics" element={<Statistics/>} />
+        <Route path="/manager" element={<ManagerApp/>} />
       </Routes>
     </Sidebar>
   );
